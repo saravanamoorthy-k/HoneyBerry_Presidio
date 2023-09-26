39bbf408c2f3b32b0d5bd9a6f0b916ca0d4fdd70
saravanamoorthy-k <mailsaravanakasi@gmail.com>
2023-09-26 18:00:55 +0530

    project initiated with required gradle dependencies

---
+++ app/src/main/java/com/presidio/honeyberry/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.presidio.honeyberry
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.presidio.honeyberry.ui.theme.HoneyBerryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HoneyBerryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HoneyBerryTheme {
+        Greeting("Android")
+    }
+}
