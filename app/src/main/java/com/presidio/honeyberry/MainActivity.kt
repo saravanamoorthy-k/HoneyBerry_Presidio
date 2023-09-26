67823752f181b5eb6662b17b90a57851adb349ae
saravanamoorthy-k <mailsaravanakasi@gmail.com>
2023-09-27 01:12:11 +0530

    working on SplashScreen. Gradle issue fixed

--- app/src/main/java/com/presidio/honeyberry/MainActivity.kt
+++ app/src/main/java/com/presidio/honeyberry/MainActivity.kt
@@ -3,44 +3,63 @@ package com.presidio.honeyberry
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.presidio.honeyberry.ui.theme.HoneyBerryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HoneyBerryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.white)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_splash_bg),
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        Image(
+            painter = painterResource(id = R.drawable.img_app_icon),
+            contentDescription = "",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(150.dp)
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewSplashScreen() {
     HoneyBerryTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
--- app/src/main/java/com/presidio/honeyberry/ui/theme/Color.kt
+++ app/src/main/java/com/presidio/honeyberry/ui/theme/Color.kt
@@ -1,11 +1,75 @@
 package com.presidio.honeyberry.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+private val GrayPrimary = Color(0xFF575757)
+private val GraySecondary = Color(0xFF949494)
+private val Background = Color(0xFFFCFCFC)
+private val Green = Color(0xFF55C595)
+private val DarkGreen = Color(0xFF289460)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightColorsPalette = HoneyBerryColors(
+    text = GraySecondary,
+    textDark = GrayPrimary,
+    textDarkGreen = DarkGreen,
+    textLight = Color.White,
+    buttonSelected = Green,
+    buttonUnselected = Color.White,
+    background = Background,
+    chipsBackground = Color.White,
+    snackBarBackground = GrayPrimary
+)
+
+@Stable
+data class HoneyBerryColors(
+    val text: Color,
+    val textDark: Color,
+    val textLight: Color,
+    val textDarkGreen: Color,
+
+    val buttonSelected: Color,
+    val buttonUnselected: Color,
+
+    val background: Color,
+    val chipsBackground: Color,
+    val snackBarBackground: Color
+)
+
+internal val LocalHoneyBerryColors = staticCompositionLocalOf { LightColorsPalette }
+
+@Composable
+internal fun ProvideHoneyBerryColors(
+    colors: HoneyBerryColors,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(LocalHoneyBerryColors provides colors, content = content)
+}
+
+/**
+ * A Material [Colors] implementation which sets all colors to [debugColor] to discourage usage of
+ * [MaterialTheme.colors] in preference to [VendorAppTheme.colors].
+ */
+internal fun debugColors(
+    darkTheme: Boolean = false,
+    debugColor: Color = Color.White
+) = Colors(
+    primary = debugColor,
+    primaryVariant = debugColor,
+    secondary = debugColor,
+    secondaryVariant = debugColor,
+    background = debugColor,
+    surface = debugColor,
+    error = debugColor,
+    onPrimary = debugColor,
+    onSecondary = debugColor,
+    onBackground = debugColor,
+    onSurface = debugColor,
+    onError = debugColor,
+    isLight = !darkTheme
+)
